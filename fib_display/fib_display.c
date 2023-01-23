ba537343b969a386928e75bea76bea5fc3ae4151
Dave Rosoff <drosoff@collegeofidaho.edu>
2023-01-23 11:59:10 -0700

    refactored, removed spurious import

--- fib_display/fib_display.c
+++ fib_display/fib_display.c
@@ -9,41 +9,42 @@
  */
 #include "pico/stdlib.h"
 #include "fib_display.h"
+#define DELAY 75
 
 void print_value(int value, uint pins[], uint num_pins) {
-
-    // for (int i = 0; i < num_pins; i++) {
-    //     gpio_put(pins[i], value & 1);
-    //     value >>= 1;
-    // }
-
+    // Only puts the value on the pins
+    // doesn't leave it
     for (int i = 0; i < num_pins; i++) {
-        gpio_put(pins[i], (value >> i) & 1);
+        uint ith_bit = value & (1 << i);
+        gpio_put(pins[i], ith_bit);
     }
 }
 
 void leave_value(int value, uint pins[], uint num_pins, uint delay) {
+    // Puts the value on the pins, then waits for delay ms.
     print_value(value, pins, num_pins);
     sleep_ms(delay);
 }
 
 void flash_value(int value, uint pins[], uint num_pins, uint delay) {
+    // Puts the value on the pins, then waits for delay ms.
+    // Then turns the pins off, then waits for delay ms.
     print_value(value, pins, num_pins);
     sleep_ms(delay);
     print_value(0, pins, num_pins);
     sleep_ms(delay);
 }
 
-uint fib(uint n) {
-    // iterative
-    uint a = 0;
-    uint b = 1;
-    for (int i = 0; i < n; i++) {
-        uint temp = a;
-        a = b;
-        b = temp + b;
+uint fib_helper(uint a, uint b, uint n) {
+    if (n == 0) {
+        return a;
+    } else {
+        return fib_helper(b, a + b, n - 1);
     }
-    return a;
+}
+
+uint fib(uint n) {
+    return fib_helper(0, 1, n);
 }
 
 int main() {
@@ -51,82 +52,44 @@ int main() {
     // stdio_init_all();
 
     // Set up the pins
-    // uint pins[] = {16, 17, 18, 19, 20, 21, 22, 26};
-    uint pins[] = {16, 17, 19, 20, 21, 22, 23, 24};
-    uint num_pins = 8; // sizeof(pins) / sizeof(pins[0]);
+    uint pins[] = {16, 17, 18, 19, 20, 21, 22, 26};
+    // uint pins[] = {16, 17, 19, 20, 21, 22, 23, 24};
+    uint num_pins = sizeof(pins) / sizeof(pins[0]);
     for (int i = 0; i < num_pins; i++) {
         gpio_init(pins[i]);
         gpio_set_dir(pins[i], GPIO_OUT);
     }
 
-    // Bea's program
-
-    while(true) {
-        for (int count = 0; count < 3; count++) {
-            leave_value(0x01, pins, num_pins, 50);
-            leave_value(0x03, pins, num_pins, 50);
-            leave_value(0x06, pins, num_pins, 50);
-            leave_value(0x0C, pins, num_pins, 50);
-            leave_value(0x18, pins, num_pins, 50);
-            leave_value(0x30, pins, num_pins, 50);
-            leave_value(0x60, pins, num_pins, 50);
-            leave_value(0xC0, pins, num_pins, 50);
-            leave_value(0x80, pins, num_pins, 100);
-            leave_value(0xC0, pins, num_pins, 50);
-            leave_value(0x60, pins, num_pins, 50);
-            leave_value(0x30, pins, num_pins, 50);
-            leave_value(0x18, pins, num_pins, 50);
-            leave_value(0x0C, pins, num_pins, 50);
-            leave_value(0x06, pins, num_pins, 50);
-            leave_value(0x03, pins, num_pins, 50);
-            leave_value(0x01, pins, num_pins, 50);
-        }
-        leave_value(0x00, pins, num_pins, 250);
-        for (int count = 0; count < 3; count++) {
-            leave_value(0xA0, pins, num_pins, 150);
-            leave_value(0x50, pins, num_pins, 150);
-            leave_value(0x28, pins, num_pins, 150);
-            leave_value(0x14, pins, num_pins, 150);
-            leave_value(0x0A, pins, num_pins, 150);
-            leave_value(0x05, pins, num_pins, 150);
-            leave_value(0x0A, pins, num_pins, 150);
-            leave_value(0x14, pins, num_pins, 150);
-            leave_value(0x28, pins, num_pins, 150);
-            leave_value(0x50, pins, num_pins, 150);
-        }
-        leave_value(0x00, pins, num_pins, 250);
-    }
-
     // Display the fib sequence
-    // uint i = 0;
-    // while (true) {
-        // uint f = fib(i++);
-        // if (f < 0x100) {
-            // flash_value(f, pins, num_pins, 250);
-        // }
-        // else {
-            // for (int count = 0; count < 3; count++) {
-                // leave_value(0x01, pins, num_pins, 40);
-                // leave_value(0x03, pins, num_pins, 40);
-                // leave_value(0x06, pins, num_pins, 40);
-                // leave_value(0x0C, pins, num_pins, 40);
-                // leave_value(0x18, pins, num_pins, 40);
-                // leave_value(0x30, pins, num_pins, 40);
-                // leave_value(0x60, pins, num_pins, 40);
-                // leave_value(0xC0, pins, num_pins, 40);
-                // leave_value(0x80, pins, num_pins, 40);
-                // leave_value(0xC0, pins, num_pins, 40);
-                // leave_value(0x60, pins, num_pins, 40);
-                // leave_value(0x30, pins, num_pins, 40);
-                // leave_value(0x18, pins, num_pins, 40);
-                // leave_value(0x0C, pins, num_pins, 40);
-                // leave_value(0x06, pins, num_pins, 40);
-                // leave_value(0x03, pins, num_pins, 40);
-                // leave_value(0x01, pins, num_pins, 40);
-            // }
-            // i = 0;
-        // }
-    // }
+    uint i = 0;
+    while (true) {
+        uint f = fib(i++);
+        if (f < 0x100) {
+            leave_value(f, pins, num_pins, 3 * DELAY);
+        }
+        else {
+            for (int count = 0; count < 1; count++) {
+                leave_value(0x01, pins, num_pins, DELAY);
+                leave_value(0x03, pins, num_pins, DELAY);
+                leave_value(0x06, pins, num_pins, DELAY);
+                leave_value(0x0C, pins, num_pins, DELAY);
+                leave_value(0x18, pins, num_pins, DELAY);
+                leave_value(0x30, pins, num_pins, DELAY);
+                leave_value(0x60, pins, num_pins, DELAY);
+                leave_value(0xC0, pins, num_pins, DELAY);
+                leave_value(0x80, pins, num_pins, DELAY);
+                leave_value(0xC0, pins, num_pins, DELAY);
+                leave_value(0x60, pins, num_pins, DELAY);
+                leave_value(0x30, pins, num_pins, DELAY);
+                leave_value(0x18, pins, num_pins, DELAY);
+                leave_value(0x0C, pins, num_pins, DELAY);
+                leave_value(0x06, pins, num_pins, DELAY);
+                leave_value(0x03, pins, num_pins, DELAY);
+                leave_value(0x01, pins, num_pins, DELAY);
+            }
+            i = 0;
+        }
+    }
 
 
     return 0;
